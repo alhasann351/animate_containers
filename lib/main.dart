e5cdd2b17eaa273f8b67177a8af1acaab95d0338
alhasann351 <alhasann352@gmail.com>
2024-03-20 03:37:01 +0600

    animated-container-added

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  var _height = 100.0;
+  var _width = 200.0;
+  bool flag = true;
+  //Color animatedContainerColor = Colors.blue[300]!;
+  Decoration _animatedContainerBoxDecoration = BoxDecoration(
+    borderRadius: BorderRadius.circular(10),
+    color: Colors.blue[300]!,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,13 +48,57 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'Animate Containers',
+            AnimatedContainer(
+              height: _height,
+              width: _width,
+              curve: Curves.easeInOutCubic,
+              decoration: _animatedContainerBoxDecoration,
+              duration: const Duration(seconds: 2),
             ),
+            const SizedBox(
+              height: 15,
+            ),
+            SizedBox(
+                height: 50,
+                width: 150,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                  onPressed: () {
+                    setState(() {
+                      if(flag){
+                        _height = 200.0;
+                        _width = 150.0;
+                        //animatedContainerColor = Colors.blue;
+                        _animatedContainerBoxDecoration = BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.blue,
+                        );
+                        flag = false;
+                      }else{
+                        _height = 100.0;
+                        _width = 200.0;
+                        //animatedContainerColor = Colors.blue[300]!;
+                        _animatedContainerBoxDecoration = BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.blue[300]!,
+                        );
+                        flag = true;
+                      }
+                    });
+                  },
+                  child: const Text(
+                    'Animate',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )),
           ],
         ),
       ),
